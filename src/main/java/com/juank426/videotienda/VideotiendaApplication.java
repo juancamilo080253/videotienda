b6f184c19a23caa931494ab92552a3d77c8bd6aa
Camilo Gomez <juancamilo080253@gmail.com>
2022-09-14 17:14:59 -0500

    first commit

---
+++ src/main/java/com/juank426/videotienda/VideotiendaApplication.java
@@ -0,0 +1,13 @@
+package com.juank426.videotienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juank426/videotienda/VideotiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juank426.videotienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
